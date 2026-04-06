48b07728ab609f8acb7f190cb79ff1097d44806e
Asad <aliasad07518@gmail.com>
2026-04-06 10:58:17 +0530

    feat(admin): update admin service configuration and exception handling

--- AdminService/Program.cs
+++ AdminService/Program.cs
@@ -3,13 +3,22 @@ using AdminService.Extensions;
 using AdminService.Middleware;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .WriteTo.File("logs/adminservice-.txt", rollingInterval: RollingInterval.Day);
+});
 
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddSwaggerDocumentation();
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.PropertyNameCaseInsensitive = true);
@@ -38,6 +47,7 @@ app.UseSwaggerUI(c =>
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
 
